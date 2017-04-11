61ecb6335d57b264b47976d0ff4535971b45cc3a
Basim Hennawi <basim.a.hennawi@gmail.com>
2017-04-11 23:13:50 +0200

    Integrate with Winston for logging errors & exceptions
    (closes #11)

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ var express 	= require('express');
 var app  		= express();
 var path 		= require('path');
 var bodyParser 	= require('body-parser');
+var flash 		= require('connect-flash');
 
 // Chat application components
 var routes 		= require('./app/routes');
+var session 	= require('./app/session');
+var passport    = require('./app/auth');
 var ioServer 	= require('./app/socket')(app);
+var logger 		= require('./app/logger');
 
 // Set the port number
 var port = process.env.PORT || 3000;
@@ -21,6 +25,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use('/', routes);
 
